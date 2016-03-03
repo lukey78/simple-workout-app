87f9484f34ac7ecd8d2137b7b862ff259aeda184
Pat Maechler <p.maechler@iwf.ch>
2016-03-03 23:35:45 +0100

    akward sound hack ;-)

--- workouts/leistungstest.js
+++ workouts/leistungstest.js
@@ -12,35 +12,47 @@ workouts.push(
         exercises: [
             {
                 exercise: "GET READY!",
-                duration: 10
+                duration: 10,
+                sound: "NASA_countdown_10sec.mp3"
             },
             {
                 exercise: "Pushups",
-                duration: 120
+                duration: 120,
+                sound: "pumper-1-8.mp3",
+                restartEvery: 30
             },
             {
                 exercise: "Pause",
-                duration: 20
+                duration: 20,
+                sound: "elevator_20sec.mp3"
             },
             {
                 exercise: "Pullups",
-                duration: 60
+                duration: 60,
+                sound: "pumper-1-8.mp3",
+                restartEvery: 30
             },
             {
                 exercise: "Pause",
-                duration: 20
+                duration: 20,
+                sound: "elevator_20sec.mp3"
             },
             {
                 exercise: "Dips",
-                duration: 60
+                duration: 60,
+                sound: "pumper-1-8.mp3",
+                restartEvery: 30
             },
             {
                 exercise: "Pause",
-                duration: 20
+                duration: 20,
+                sound: "elevator_20sec.mp3"
             },
             {
                 exercise: "Burpees",
-                duration: 120
+                duration: 120,
+                sound: "pumper-1-8.mp3",
+                restartEvery: 30
             }
         ]
     }
